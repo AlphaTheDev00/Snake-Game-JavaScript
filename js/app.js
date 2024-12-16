3dfbe9f0ec87bb969ea8f9444405cd42cd58987f
AlphaTheDev00 <chikar.yassine@gmail.com>
2024-12-16 14:31:45 +0000

    Changing Velocity value based ok key press

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 
 let appleX, appleY;
+let snakeX = 5, snakeY = 10;
 
 const changeApplePosition = () => {
 
@@ -9,8 +10,23 @@ const changeApplePosition = () => {
     appleY = Math.floor(Math.random() * 30) + 1;
 }
 
+const ControlDirection = (e) => {
+    //Changing Velocity value based ok key press
+    if(e.key === "ArrowUp")  {
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "Arrowleft") {
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
 const startGame = () => {
     let htmlMarkup = `<div class="apple" style="grid-area: ${appleY} / ${appleX}"></div>`;
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 changeApplePosition();
